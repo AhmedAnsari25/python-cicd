1b7425830b12a70234c75eb3ffdd42c9f30d5dc0
Riz Ahmed <riz@Ahmed.Riz>
2024-08-26 06:19:52 -0400

    Initial commit with app and test files

---
+++ test_app1.py
@@ -0,0 +1,6 @@
+# test_app.py
+import subprocess
+
+def test_output():
+    result = subprocess.run(["python3", "app.py"], capture_output=True, text=True)
+    assert "Hello, World!" in result.stdout
